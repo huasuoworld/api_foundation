5035032120110283ab46c26b9b903d8e5b765b43
huacailiang <2542002606@qq.com>
2022-06-20 15:24:20 +0800

    1 validation function input parameter

--- src/main/java/org/huasuoworld/function/impl/FunctionExecuteImpl.java
+++ src/main/java/org/huasuoworld/function/impl/FunctionExecuteImpl.java
@@ -1,6 +1,8 @@
 package org.huasuoworld.function.impl;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.ComposedSchema;
+import io.swagger.v3.oas.models.media.Schema;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -9,6 +11,8 @@ import org.huasuoworld.function.FunctionExecute;
 import org.huasuoworld.function.FunctionFactory;
 import org.huasuoworld.input.OpenAPIBuilder;
 import org.huasuoworld.models.Function;
+import org.huasuoworld.resource.Operations;
+import org.huasuoworld.resource.Schemas;
 
 /**
  * @author: huacailiang
@@ -35,11 +39,41 @@ public class FunctionExecuteImpl implements FunctionExecute {
 
   @Override
   public Map<String, Object> exec(Function function) {
+    Map<String, Object> executeMap = new HashMap<>();
     try {
       OpenAPI openAPI = function.getOpenAPI();
+      Optional<String> inputOpt = OpenAPIBuilder.getVariables(Optional.ofNullable(openAPI), "input");
+      if(!inputOpt.isPresent()) {
+        return executeMap;
+      }
+      Schema schema = openAPI.getComponents().getSchemas().get(inputOpt.get());
+      //step1 filter parameter map
+      Map<String, Object> payload = function.getPayload();
+      if(!ObjectUtils.isEmpty(schema.getAllOf()) && !schema.getAllOf().isEmpty()) {
+        ComposedSchema composedSchema = (ComposedSchema) schema;
+        Map<String, Object> properties = composedSchema.getAllOf().get(0).getProperties();
+        if(!ObjectUtils.isEmpty(properties) && !properties.isEmpty()) {
+          Map<String, Object> payloadVerified = new HashMap<>();
+          properties.keySet().stream().forEach(key -> {
+            payloadVerified.put(key, payload.get(key));
+          });
+          //reload payload
+          function.setPayload(payloadVerified);
+        }
+      } else {
+        Map<String, Object> properties = schema.getProperties();
+        if(!ObjectUtils.isEmpty(properties) && !properties.isEmpty()) {
+          Map<String, Object> payloadVerified = new HashMap<>();
+          properties.keySet().stream().forEach(key -> {
+            payloadVerified.put(key, payload.get(key));
+          });
+          //reload payload
+          function.setPayload(payloadVerified);
+        }
+      }
       Optional<String> classPathOpt = OpenAPIBuilder.getVariables(Optional.ofNullable(openAPI), "classPath");
       if(!classPathOpt.isPresent()) {
-        return new HashMap<>();
+        return executeMap;
       }
       String classPath = classPathOpt.get();
       FunctionFactory factory = FunctionFactory.getInstance();
@@ -49,14 +83,14 @@ public class FunctionExecuteImpl implements FunctionExecute {
         factory.put(function.getName(), executeFunction);
       }
       if(ObjectUtils.isEmpty(factory.get(function.getName()))) {
-        return new HashMap<>();
+        return executeMap;
       }
       //step3 run function
       Map<String, Object> applyMap = (Map<String, Object>) factory.get(function.getName()).apply(function);
       return applyMap;
     } catch (Exception e) {
       e.printStackTrace();
-      return new HashMap<>();
+      return executeMap;
     }
   }
 }
