d3c43a27ea6ab5a134328f4cc664bb7073f67aeb
huacailiang <2542002606@qq.com>
2022-06-20 13:30:11 +0800

    1 add error message constant

--- src/main/java/org/huasuoworld/output/Constant.java
+++ src/main/java/org/huasuoworld/output/Constant.java
@@ -6,17 +6,7 @@ package org.huasuoworld.output;
  * @description:
  **/
 public class Constant {
-  public static final String SUCCESS = "成功";
-  public static final String FAIL = "失败";
-  public static final String CANCEL = "取消";
-  public static final String EXCEPTION = "程序异常";
-  public static final String TIMEOUT = "请求超时";
-  public static final String TIMEOUT_PROTECT = "请求超时，缓冲保护";
-  public static final String RE_SUBMIT = "重复提交";
-  public static final String ILLEGAL_SUBMIT = "非法提交";
-  public static final String SUBMIT_CONFLICT = "请勿重复提交，稍后重试";
-  public static final String INSURANCE_BASIC_INFO_EMPTY = "保险公司接口为空";
-  public static final String NOT_ALLOW = "请求不允许";
-  public static final String VERIFY_PASS = "验证签名通过";
-  public static final String VERIFY_NOT_PASS = "验证签名不通过";
+  public static final String SUCCESS = "successful";
+  public static final String FAIL = "fail";
+  public static final String OPEN_API_NOT_FOUND = "open api not found";
 }
